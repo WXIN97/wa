7b37df4ddc1c9505411c23b083ada34a92619cb0
wang <wang@qq.com>
2022-07-29 21:51:55 +0800

    a

--- src/main/java/appliac.java
+++ src/main/java/appliac.java
@@ -1,15 +1,18 @@
+import javax.xml.ws.WebServiceProvider;
+
 /**
  * @Description:
  * @Author: 鑫
  * @Date: 2022-07-29 17:54
  */
-
+@WebServiceProvider
 public class appliac {
     public static void main(String[] args) {
         System.out.println("************");
         System.out.println("************");
         System.out.println("************");
         System.out.println("***********呜呜呜呜*");
+        System.out.println("***********aaaaaaaa呜*");
         int a;
     }
 }
