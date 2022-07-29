67c6647391cb8f4720c2f37c3b96f570e49ace3d
wang <wang@qq.com>
2022-07-29 18:14:38 +0800

    Initial commit

---
+++ src/main/java/appliac.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @Author: 鑫
+ * @Date: 2022-07-29 17:54
+ */
+
+public class appliac {
+    public static void main(String[] args) {
+        System.out.println("************");
+    }
+}
